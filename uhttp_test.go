7765f00c60dd14052892f349e07e88b81d24f7b9
John Krukoff <john@krukoff.us>
2015-03-03 15:22:07 -0700

    [master]: Change Error signature to take error directly.
    This changes uhttp.Error to take the error object directly, and extract
    the string from it via Error(). This should allow for more flexibility
    to accept different kinds of errors in the future, and also more closely
    matches the 90% use case.
    
    Additionally, added a Status write method that constructs an appropriate
    body from the status code alone, in order to simplify the common 204 &
    403 use cases in the Utopia code.

--- uhttp_test.go
+++ uhttp_test.go
@@ -1,6 +1,7 @@
 package uhttp
 
 import (
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -14,7 +15,7 @@ type sample struct {
 	Hello string `json:"hello"`
 }
 
-func TestParseJson(t *testing.T) {
+func TestRead(t *testing.T) {
 	body := strings.NewReader(`{"hello":"world"}`)
 	r := &http.Request{
 		Body: ioutil.NopCloser(body),
@@ -25,12 +26,52 @@ func TestParseJson(t *testing.T) {
 	assert.Equal(t, b.Hello, "world", "Hello should contain 'world'")
 }
 
-func TestJsonWriter(t *testing.T) {
+func TestWrite(t *testing.T) {
 	b := sample{
 		Hello: "world",
 	}
 	w := httptest.NewRecorder()
 	err := Write(w, b, 200)
 	assert.Nil(t, err)
-	assert.Equal(t, w.Body.String(), `{"hello":"world"}`, "should be equal to valid json string")
+	assert.Equal(t, w.Code, 200, "should be expected status code")
+	assert.Equal(
+		t,
+		w.Body.String(),
+		`{"hello":"world"}`,
+		"should be equal to valid json string")
+}
+
+func TestError(t *testing.T) {
+	e := errors.New("test")
+	w := httptest.NewRecorder()
+	err := Error(w, e, 200)
+	assert.Nil(t, err)
+	assert.Equal(t, w.Code, 200, "should be expected status code")
+	assert.Equal(
+		t,
+		w.Body.String(),
+		`{"error":"test"}`,
+		"should be formatted as JSON error")
+}
+
+func TestStatus(t *testing.T) {
+	w := httptest.NewRecorder()
+	err := Status(w, 403)
+	assert.Nil(t, err)
+	assert.Equal(t, w.Code, 403, "should be expected status code")
+	assert.Equal(
+		t,
+		w.Body.String(),
+		`{"error":"Forbidden"}`,
+		"should be formatted as JSON error")
+
+	w = httptest.NewRecorder()
+	err = Status(w, 204)
+	assert.Nil(t, err)
+	assert.Equal(t, w.Code, 204, "should be expected status code")
+	assert.Equal(
+		t,
+		w.Body.String(),
+		"",
+		"should be an empty response")
 }
