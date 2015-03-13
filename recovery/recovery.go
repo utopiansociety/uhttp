a792776a6709edda277a35a693dbb36c9811b5c6
Austin Riendeau <ariendeau@itriagehealth.com>
2015-03-13 14:18:27 -0600

    removes really bad logging

--- recovery/recovery.go
+++ recovery/recovery.go
@@ -9,16 +9,10 @@ import (
 func Handler(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
-			log.Println("RECOVERY START")
 			if err := recover(); err != nil {
-				log.Println("RECOVERY ERROR")
-				//log.Panic(err)
 				log.Printf("panic: %#v", err)
 				w.WriteHeader(500)
-				w.Write([]byte("hello!!!"))
-				log.Println("lalalalala")
 			}
-			log.Println("RECOVERY END")
 		}()
 		next.ServeHTTP(w, r)
 	}
