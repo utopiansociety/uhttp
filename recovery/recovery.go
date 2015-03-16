855a7fb6cca56c58beef0beecba84e60c1000a17
Austin Riendeau <ariendeau@itriagehealth.com>
2015-03-16 11:14:57 -0600

    adds debug wrapper

--- recovery/recovery.go
+++ recovery/recovery.go
@@ -3,17 +3,22 @@ package recovery
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 // Handler is how to handle errors right now.
 func Handler(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if err := recover(); err != nil {
-				log.Printf("panic: %#v", err)
-				w.WriteHeader(500)
-			}
-		}()
+		debug := os.Getenv("DEBUG")
+		if debug == "" {
+			defer func() {
+				if err := recover(); err != nil {
+					log.Printf("panic: %#v", err)
+					w.WriteHeader(500)
+				}
+			}()
+
+		}
 		next.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
