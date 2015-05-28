0dfbe4974c67d68e6153723cd48a155523f9f380
Austin Riendeau <ariendeau@itriagehealth.com>
2015-05-28 14:57:18 -0600

    header fix

--- uhttp.go
+++ uhttp.go
@@ -20,12 +20,12 @@ func Read(r *http.Request, body interface{}) error {
 // Write serializes an interface to JSON and writes out the headers, given
 // status code and serialized data to the given ResponseWriter.
 func Write(w http.ResponseWriter, body interface{}, status int) error {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	j, err := json.Marshal(body)
 	if err != nil {
 		return err
 	}
-	w.Header().Set("Content-Type", "application/json")
 	_, err = w.Write(j)
 	return err
 }
